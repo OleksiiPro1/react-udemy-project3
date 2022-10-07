515d040b6f745e68533090d6273f41c8940891ce
Alexey Piltenko <101993475+OleksiiPro1@users.noreply.github.com>
2022-10-07 10:27:42 +0200

    Finish

--- src/components/Users/AddUser.js
+++ src/components/Users/AddUser.js
@@ -7,15 +7,24 @@ import classes from './AddUser.module.css';
 const AddUser = (props) => {
   const [enteredUsername, setEnteredUsername] = useState('');
   const [enteredAge, setEnteredAge] = useState('');
+  const [error, setError] = useState();
 
   const addUserHandler = (event) => {
     event.preventDefault();
 
     if (enteredUsername.trim().length === 0 || enteredAge.trim().length === 0) {
+      setError({
+        title: 'invalid input',
+        message: 'Please enter a valid name and age',
+      });
       return;
     }
 
     if (+enteredAge < 1) {
+      setError({
+        title: 'invalid input',
+        message: 'Please enter a valid age',
+      });
       return;
     }
     // console.log(enteredUsername, enteredAge);
@@ -32,9 +41,18 @@ const AddUser = (props) => {
     setEnteredAge(event.target.value);
   };
 
+  const errorHandler = () => {
+    setError(null);
+  };
   return (
     <div>
-      <ErrorModal title="An error occured!" message="Something went wrong!" />
+      {error && (
+        <ErrorModal
+          title={error.title}
+          message={error.message}
+          onConfirm={errorHandler}
+        />
+      )}
       <Card className={classes.input}>
         <form onSubmit={addUserHandler}>
           <label htmlFor="username">User Name</label>
